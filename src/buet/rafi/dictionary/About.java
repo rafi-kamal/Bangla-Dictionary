1cc292b5da4ccb74413f6bcabcb80a356f73ab19
Rafi Kamal <rafikamal93@gmail.com>
2013-10-12 08:17:55 +0600

    Create About.java

---
+++ src/buet/rafi/dictionary/About.java
@@ -0,0 +1,19 @@
+package buet.rafi.dictionary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+  This is just a place holder
+*/
+public class About extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+	}
+}
