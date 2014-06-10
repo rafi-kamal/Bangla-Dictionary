ada985afb616c06abe1da2e774bbe381144dca20
Rafi Kamal <rafikamal93@gmail.com>
2014-06-10 09:42:58 +0600

    database edited

--- src/buet/rafi/dictionary/About.java
+++ src/buet/rafi/dictionary/About.java
@@ -1,11 +1,7 @@
 package buet.rafi.dictionary;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 /**
   This is just a place holder
